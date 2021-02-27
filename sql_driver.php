70a0ef1740a6f7a9efa6efde41f6625434d54e7b
Zeljko Ivanovic <zeljkoivanovic369@gmail.com>
2021-02-27 15:34:40 +0100

    Update sql_driver.php

--- sql_driver.php
+++ sql_driver.php
@@ -3,7 +3,8 @@
 if(file_exists('config.conf')){
 	$db = unserialize(file_get_contents('config.conf'));
 }else{
-	die('Sistem is not installed');
+	//die('Sistem is not installed');
+	header('Location:setup/index.php');
 }
 
  $db_conn = mysqli_connect($db['db_host'],$db['username'],$db['password'],$db['db_name']);
